6149ad7b2e73fbd3d84428706993e0d10add1e77
Nho094 <ngwxanhphannho@gmail.com>
2026-06-25 23:41:49 +0700

    1

--- 2230C.py
+++ 2230C.py
@@ -1,43 +1,36 @@
-import sys
-
+import sys 
 def solve():
     input = sys.stdin.read
-    data = input().split()
-    
-    if not data:
-        return
-    
-    multTestQ = int(data[0])
+    data  = input().split()
+
+    if not data: 
+        return 
+    m = int(data[0])
+
     idx = 1
-    
     out = []
-    for _ in range(multTestQ):
-        n = int(data[idx])
-        c = [int(x) for x in data[idx+1 : idx+1+n]]
-        idx += 1 + n
-        
-        total_cards = sum(c)
+    for _ in range(m): 
+        n = int (data[idx])
+        h = [int(x) for x in data[idx + 1: idx + n + 1]]
+        idx += n + 1
+
+        total = sum(h)
         ones = 0
         slots = 0
-        
-        for val in c:
-            if val == 1:
+        for val in h: 
+            if val == 1: 
                 ones += 1
-            else:
+            else: 
                 slots += (val // 2) - 1
-                
-        if ones == n - 1:
+        if ones == n -1: 
             slots += 1
-            
-        wasted = max(0, ones - slots)
-        ans = total_cards - wasted
         
-        if ans < 3:
+        wasted = max(0, ones - slots)
+        ans = total - wasted
+        if ans < 3: 
             out.append("0")
-        else:
+        else: 
             out.append(str(ans))
-            
-    print('\n'.join(out))
-
+    print("\n".join(out))
 if __name__ == '__main__':
     solve()
